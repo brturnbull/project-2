dd03153416c7ed1666cc7d3c0e2f35474c4ced8d
Bridget <brturnbul@gmail.com>
2018-04-27 09:58:23 +0100

    preparing for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const databaseURI = 'mongodb://localhost/insta-spam';
-const port = 3000;
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/insta-spam';
+const port = process.env.PORT || 3000;
 
 module.exports = {databaseURI, port};
